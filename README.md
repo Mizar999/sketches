d8ef2c13cb1165be33ccec66272d827afd03c780
Mario <mizar2502@aol.com>
2022-11-22 00:48:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Sketches
+
+```powershell
+npm install
+```
+
+```powershell
+npm run build
+```
+
+```powershell
+# if globally installed
+concurrently npm:watch npm:serve
+
+# if locally installed
+npx concurrently npm:watch npm:serve
+```
+
+```powershell
+npm init -y
+npm install --save-dev typescript@4.9.3 ts-loader@9.4.1 p5@1.5.0 @types/p5@1.4.3 webpack@5.75.0 webpack-cli@5.0.0 http-server@14.1.1 concurrently@7.6.0
+```
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        basic1: ['./src/basic1/app.ts']
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development'
+};
