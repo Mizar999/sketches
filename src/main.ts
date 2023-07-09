2f039dd9de1aeecaa16ac2d1a1c0903400fa5e7e
Mario <mizar2502@aol.com>
2023-07-09 18:11:25 +0200

    Add session storage to simplify development with live server

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,28 @@ import { simpleShapes, shapeSketch } from "./shapes";
 
 class SketchManager {
     private mainP5: P5;
-    private sketches: {[key: string]: (p5: P5) => void};
+    private sketches: { [key: string]: (p5: P5) => void };
 
     constructor(id: string) {
         this.populateSketches();
 
         let select = document.getElementById(id) as HTMLSelectElement;
         let option;
-        for(let key of Object.keys(this.sketches)) {
+        for (let key of Object.keys(this.sketches)) {
             option = document.createElement("option");
             option.text = key;
             select.add(option);
         }
+        
+        if (sessionStorage.getItem("selectedSketch")) {
+            select.value = sessionStorage.getItem("selectedSketch");
+            this.changeSketch(select.value);
+        }
 
-        select.addEventListener("change", () => this.changeSketch(select.value));
+        select.addEventListener("change", () => {
+            this.changeSketch(select.value);
+            sessionStorage.setItem("selectedSketch", select.value);
+        });
     }
 
     private populateSketches() {
